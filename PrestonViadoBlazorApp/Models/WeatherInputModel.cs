bf974018896049b7a3fa99405586eea452dfc432
PrestonV <prestonviado@outlook.com>
2024-02-02 23:47:16 -0500

    Implemented weather app

---
+++ PrestonViadoBlazorApp/Models/WeatherInputModel.cs
@@ -0,0 +1,6 @@
+﻿namespace PrestonViadoBlazorApp.Models;
+
+public class WeatherInputModel
+{
+    public string? LatLong { get; set; }
+}
--- PrestonViadoBlazorApp/Program.cs
+++ PrestonViadoBlazorApp/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient(); // Needed for weather app
 
 var app = builder.Build();
 
